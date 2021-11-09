4c9fc41e851bd0b82bede77357c41981888af0e3
Opilane TTHK <opilane@tthk.ee>
2021-11-09 15:51:28 +0200

    first commit

---
+++ Colors/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Colors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select one color: (red,Blue, Green, Yellow)");
+            string userColor = Console.ReadLine();
+            Console.WriteLine(GetColors(userColor));
+        }
+        public static string[] GetPersonality()
+        {
+            string filepath = @"C:\Users\opilane\samples\personality.txt";
+            string [] DataFromFile
+        }
+        }
+    }
+}
